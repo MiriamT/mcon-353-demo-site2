23a552cfba8c2fe460b3b3db36c392631a7a576e
MiriamT <MiriamT@users.noreply.github.com>
2022-02-17 19:47:03 -0500

    add basic todo

--- src/components/todo/todo.jsx
+++ src/components/todo/todo.jsx
@@ -1,6 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./todo.css";
 
-export const Todo = () => {
-  return <h1 className="todo">Todo</h1>;
+export const Todo = (props) => {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function addTodo() {
+    const newTodos = [...todos, inputText];
+    setTodos(newTodos);
+  }
+
+  function deleteTodo(deletedTodo) {
+    setTodos(todos.filter((todo) => todo !== deletedTodo));
+  }
+
+  return (
+    <div>
+      <input
+        type="text"
+        onChange={(event) => setInputText(event.target.value)}
+      />
+      <button onClick={addTodo}>Add</button>
+      {todos.map((todo) => (
+        <TodoItem text={todo} deleteTodo={deleteTodo} />
+      ))}
+    </div>
+  );
+};
+
+const TodoItem = (props) => {
+  return (
+    <div>
+      {props.text}
+      <button onClick={() => props.deleteTodo(props.text)}>delete</button>
+    </div>
+  );
 };
