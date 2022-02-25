5339236c33c318538928f741afdbe523830b460d
MiriamT <MiriamT@users.noreply.github.com>
2022-02-24 19:19:18 -0500

    add header and router

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,14 +1,21 @@
 import "./App.css";
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { Home } from "../home/home";
 import { Todo } from "../todo/todo";
+import { Header } from "../header/header";
 
 function App() {
   return (
     <div>
-      <Home /> {/* you can comment out the component you are not working on */}
-      <Todo />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/todo" element={<Todo />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
